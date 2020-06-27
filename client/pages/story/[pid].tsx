3a0f12fd9c870a9c3b771f1e7690742876d8e153
VD <vico77@live.fr>
2020-06-27 08:53:18 +0200

    Update story page style

--- client/pages/story/[pid].tsx
+++ client/pages/story/[pid].tsx
@@ -4,23 +4,29 @@ import { getStory, getStories } from '../../src/utils/Api';
 
 import Config from '../../src/Config';
 import Navigation from '../../src/components/navigation';
-import Header from '../../src/components/Header';
 import Footer from '../../src/components/Footer';
 import { css } from 'emotion';
 
-const FormStyle = css`
+const containerStyle = css`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  min-height: 100%;
+`;
+
+const formStyle = css`
   background-color: rgba(99, 93, 93, 0.7);
-  border-radius: 2px;
-  padding: 5px;
+  border-radius: 5px;
+  padding: 10px;
 `;
 
 const reactionsBox = css`
   position: absolute;
   top: -2rem;
-  right: 0;
+  left: 0;
 `;
 
-const postStyle = css`
+const storyStyle = css`
   position: relative;
   border-bottom: 1px solid black;
 
@@ -31,16 +37,38 @@ const postStyle = css`
   }
 `;
 
-const CommentStyle = css`
-  background-color: rgba(255, 255, 255, 0.7);
+const commentStyle = css`
+  background-color: rgba(114, 214, 236, 0.7);
   border-radius: 6px;
   border-bottom: 1px solid black;
   padding: 6px;
+  width: 100%;
+  min-width: 100%;
+  max-width: 100%;
 `;
 
 const submitStyle = css`
   position: absolute;
+  bottom: 10px;
+  right: 9px;
 `;
+
+export const toDate = (unix_timestamp) =>{
+  // Create a new JavaScript Date object based on the timestamp
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  const date = new Date(unix_timestamp * 1000);
+  // Hours part from the timestamp
+  const hours = date.getHours();
+  // Minutes part from the timestamp
+  const minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  const seconds = "0" + date.getSeconds();
+
+  // Will display time in 10:30:23 format
+  const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+  return formattedTime;
+}
 
 class Story extends React.Component {
   constructor(props: void) {
@@ -48,8 +76,10 @@ class Story extends React.Component {
 
     this.state = {
       comments: ['com1', 'com2'],
+      comment: '',
     };
 
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -66,16 +96,20 @@ class Story extends React.Component {
 
   async componentDidMount() {}
 
+  handleChange(event) {
+    this.setState({comment: event.target.comment});
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    console.log('A comment was submitted: ' + this.state.comment);
   }
 
   render() {
     const { story } = this.props;
-    const comments = ['com1', 'com2'].map((item, index) => (
+    const comments = ['com1', 'com2', 'com3'].map((item, index) => (
       <div>
-        <p className={CommentStyle} key={index}>
+        <p className={commentStyle} key={index}>
           {item}
         </p>
       </div>
@@ -90,32 +124,31 @@ class Story extends React.Component {
           />
         </Head>
         <Navigation />
-
-        <form
-          className={`${FormStyle} col-12 col-md-6 col-lg-6 `}
-          onSubmit={this.handleSubmit}
-        >
-          <div className={postStyle}>
-            <p>Date: {story.date}</p>
-            <p>{story.content}</p>
-            <div className={reactionsBox}>
-              <span className="fas fa-surprise" />
-              <span className="fas fa-surprise" />
-              <span className="fas fa-surprise" />
+        <div className={containerStyle}>
+          <form
+            className={`${formStyle} col-12 col-md-6 col-lg-6 `}
+            
+          >
+            <div className={storyStyle}>
+              <p className={css`text-align: right;`}>{toDate(story.date)}</p>
+              <p className={css`text-overflow: ellipsis; overflow: hidden;`}>{story.content}</p>
+              <div className={reactionsBox}>
+                <span className="fas fa-surprise" />
+                <span className="fas fa-surprise" />
+                <span className="fas fa-surprise" />
+              </div>
             </div>
-          </div>
 
-          <div>{comments}</div>
-          <div>
-            <input className={CommentStyle} type="text" />
-
-            <span
-              className={` ${submitStyle} far fa-paper-plane`}
-              onClick={this.handleSubmit}
-            />
-          </div>
-        </form>
-
+            <div>{comments}</div>
+            <div className={css`position: relative;`}>
+              <textarea onChange={this.handleChange} className={commentStyle} />
+              <span onClick={this.handleSubmit}
+                className={`${submitStyle} far fa-paper-plane`}
+                
+              />
+            </div>
+          </form>
+        </div>
         <Footer />
       </>
     );
@@ -123,55 +156,3 @@ class Story extends React.Component {
 }
 
 export default Story;
-
-// const Post = ({ story }) => {
-//   const router = useRouter();
-//   const { pid } = router.query;
-
-//   console.log('story', story);
-
-//   return (
-//     <p>
-//       Post: {pid} {story._id || 'Nothing'}
-//     </p>
-//   );
-// };
-
-// export async function getStaticPaths() {
-//   console.log('getStaticPaths');
-//   const stories = await getStories();
-//   // const posts = await res.json();
-
-//   // Get the paths we want to pre-render based on posts
-//   const paths = stories.map((story) => {
-//     console.log('#getStaticPaths story', story);
-//     return {
-//       params: { pid: story._id },
-//     };
-//   });
-//   console.log('paths', paths);
-
-//   // We'll pre-render only these paths at build time.
-//   // { fallback: false } means other routes should 404.
-//   return { paths, fallback: true };
-//   // ...
-// }
-
-// export async function getStaticProps(context) {
-//   console.log('getStaticProps');
-//   console.log('context', context);
-//   // Call an external API endpoint to get posts.
-//   // You can use any data fetching library
-//   const story = await getStory({ id: context.params.pid });
-//   // const posts = await res.json();
-
-//   // By returning { props: posts }, the Blog component
-//   // will receive `posts` as a prop at build time
-//   return {
-//     props: {
-//       story,
-//     },
-//   };
-// }
-
-// export default Post;
